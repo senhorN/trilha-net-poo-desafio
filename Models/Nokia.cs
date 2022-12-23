503172c23de581b6bf09b04def48da23a90f2c83
Nicolas Carloto <90424448+senhorN@users.noreply.github.com>
2022-12-23 17:26:44 -0300

    [+] Nokia

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone //Herdado Smartphone na classe Nokia
+
     {
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public void Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Aplicativo {nomeApp} instalando no Nokia com sucesso!");
+        }
     }
 }

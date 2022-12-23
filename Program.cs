a47e871f46822c3d4f643f103d890b8149432ced
Nicolas Carloto <90424448+senhorN@users.noreply.github.com>
2022-12-23 17:25:10 -0300

    [+] solução

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+
+Nokia nokia = new Nokia(numero: "123", modelo: "PEDRA", imei: "001020011", memoria: 8);
+nokia.Ligar();
+nokia.InstalarAplicativo("Ligue of Legends: Wild rift");
+
+Console.WriteLine("Smartphone Iphone:");
+
+Iphone iphone = new Iphone(numero: "454465789", modelo: "Roxo", imei: "15601100", memoria: 512);
+iphone.Ligar();
+iphone.InstalarAplicativo("Mobile Legends);
